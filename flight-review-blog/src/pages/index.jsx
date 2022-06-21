9563dcbbd57dce5b3437851ec2755b8f53c43279
zach <ztbochanski@gmail.com>
2022-06-20 20:29:55 -0700

    add index jsx and import scss

---
+++ flight-review-blog/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import "../index.scss"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+  </Layout>
+)
+
+export default IndexPage
