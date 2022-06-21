9ab3ae786e122e7ec28d4b97a910d42778dc3d80
zach <ztbochanski@gmail.com>
2022-06-21 12:50:19 -0700

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Layout from "../hoc/layout"
+import { StaticImage } from "gatsby-plugin-image"
+
+const PageNotFound = () => (
+  <Layout>
+      <div className="section">
+        <div className="notification is-danger mb-0">
+          <h1 className="title">404: Not Found</h1>
+          <h2 className="subtitle">Unicorn Pusheen</h2>
+        </div>
+        <div className="block">
+          <StaticImage
+            src="../images/pusheen_placeholder.gif"
+            alt="A unicorn pusheen"
+            placeholder="blurred"
+            layout="fullWidth"
+          />
+        </div>
+      </div>
+  </Layout>
+)
+
+export default PageNotFound
