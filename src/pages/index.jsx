cbe010116b62566ad543f9544ae8ce7c5dc1f8ae
zach <ztbochanski@gmail.com>
2022-06-22 13:26:03 -0700

    remove link content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,12 @@
 import React from "react"
 import Seo from "../components/seo"
 import "../index.scss"
-import { useMarkdownNodes } from "../hooks/useMarkdownNodes.js"
-import { Link } from "gatsby"
 
-const IndexPage = () => {
-  const nodes = useMarkdownNodes()
-  return(
-    <div>
-      <Seo title="Home" />
-      {nodes.map((node) => (
-        <div key={node.id}>
-          <Link to={node.slug}>{node.frontmatter.title}</Link>
-        </div>
-      ))}
-    </div>
-  )
-}
+const IndexPage = () => (
+  <div>
+    <Seo title="Home" />
+    <h1>IndexPage</h1>
+  </div>
+)
 
 export default IndexPage
