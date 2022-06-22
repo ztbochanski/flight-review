c6a993f86d7cd8be99cb728394ef96b3d0aa3a9b
zach <ztbochanski@gmail.com>
2022-06-22 11:09:28 -0700

    add onCreate node for mdx and md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/content",
-    component: require.resolve("./src/templates/content.js"),
-    context: {},
-    defer: true,
-  })
+const replacePath = require("./gatsby/utils.js")
+
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/content",
+//     component: require.resolve("./src/templates/content.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: replacePath(slug),
+    })
+  } else if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: 'slug',
+      node,
+      value: replacePath(value),
+    })
+  }
 }
