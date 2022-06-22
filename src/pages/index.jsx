94808c55d98d7e824d81db5d8463eaf8b8dbaf63
zach <ztbochanski@gmail.com>
2022-06-22 12:27:59 -0700

    add node query hook and display mdx nodes as links to their repective page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,21 @@
 import React from "react"
 import Seo from "../components/seo"
 import "../index.scss"
+import { useMarkdownNodes } from "../hooks/useMarkdownNodes.js"
+import { Link } from "gatsby"
 
-const IndexPage = () => (
-    <Seo title="Home" />
-)
+const IndexPage = () => {
+  const nodes = useMarkdownNodes()
+  return(
+    <div>
+      <Seo title="Home" />
+      {nodes.map((node) => (
+        <div key={node.id}>
+          <Link to={node.slug}>{node.frontmatter.title}</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default IndexPage
