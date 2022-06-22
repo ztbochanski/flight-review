9dd9318f10090ce461ea617c2df69844478d442e
zach <ztbochanski@gmail.com>
2022-06-22 12:29:52 -0700

    refactor to remove extra components

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,83 +1,17 @@
 import React from "react"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import PropTypes from "prop-types"
-import Header from "/src/components/header.jsx"
-import Footer from "/src/components/footer.jsx"
-
-const renderItem = (item) => {
-
-  if (item) {
-    return(
-      <li key={item.link}>
-        <Link to={item.link}>
-          <div>{item.name}</div>
-        </Link>
-
-        {item.subMenu && item.subMenu.length > 0 ? (
-          <ul>
-            {item.subMenu.map((subLink) => (
-              <li key={subLink.name}>
-                <Link to={subLink.link}>
-                  {subLink.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : null}
-
-      </li>
-    )
-  } else {
-    return null
-  }
-}
-
-const SideBar = ({ menuLinks }) => {
-
-  // const [isActive, setIsActive] = React.useState(false)
-
-  return(
-    <aside className="menu">
-      <p className="menu-label">
-        General
-      </p>
-      <ul className="menu-list">
-        {menuLinks.map((link) => renderItem(link))}
-      </ul>
-    </aside>
-  )
-}
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
+import Header from "/src/components/header"
+import Footer from "/src/components/footer"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          menuLinks {
-            name
-            link
-            subMenu {
-              name
-              link
-            }
-          }
-        }
-      }
-    }
-  `)
+  const { title } = useSiteMetadata()
   return(
     <div>
-      <Header siteTitle={data.site.siteMetadata.title || `Flying is Rad`} />
-      <SideBar menuLinks={data.site.siteMetadata.menuLinks} />
+      <Header siteTitle={title || `Flying is Rad`} />
       {children}
-      <Footer siteTitle={data.site.siteMetadata.title || `Flying is Rad`} />
+      <Footer siteTitle={title || `Flying is Rad`} />
     </div>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
