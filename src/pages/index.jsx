0cb33bfb095a25dda8fd913bc689c2a9e7a94480
zach <ztbochanski@gmail.com>
2022-06-22 09:44:13 -0700

    remove explicit layout now implemented via plugin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,9 @@
 import React from "react"
-
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../index.scss"
 
 const IndexPage = () => (
-  <Layout>
     <Seo title="Home" />
-  </Layout>
 )
 
 export default IndexPage
