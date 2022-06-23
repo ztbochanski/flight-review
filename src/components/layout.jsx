3cd35b6c43a209896dc7b3301157b15aafe2a402
zach <ztbochanski@gmail.com>
2022-06-22 22:10:04 -0700

    refactor into multiple layouts

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,7 +4,18 @@ import Header from "./header"
 import Footer from "./footer"
 import SideBar from "./sidebar"
 
-const Layout = ({ children }) => {
+const IndexLayout = ({ children }) => {
+  const { title } = useSiteMetadata()
+  return(
+    <div>
+      <Header siteTitle={title || `Flying is Rad`} />
+      {children}
+      <Footer siteTitle={title || `Flying is Rad`} />
+    </div>
+  )
+}
+
+const RegularLayout = ({ children }) => {
   const { title } = useSiteMetadata()
   return(
     <div>
@@ -23,6 +34,33 @@ const Layout = ({ children }) => {
       <Footer siteTitle={title || `Flying is Rad`} />
     </div>
   )
+  // return(
+  //   <div>
+  //     <Header siteTitle={title || `Flying is Rad`} />
+  //     <div className="columns">
+  //       <div className="column is-narrow" style={{borderRight: "1px solid lightgrey"}}>
+  //         <SideBar />
+  //       </div>
+  //       <div className="column">
+  //         <div className="section">
+  //           <div className="container">{children}</div>
+  //         </div>
+  //       </div>
+        
+  //     </div>
+  //     <Footer siteTitle={title || `Flying is Rad`} />
+  //   </div>
+  // )
+}
+
+
+const Layout = ({ children, pageContext }) => {
+  console.log(pageContext.layout);
+  if (pageContext.layout === "index") {
+    return <IndexLayout children={children}></IndexLayout>
+  }
+  return <RegularLayout children={children}></RegularLayout>
+  
 }
 
 export default Layout
