03c53fe8ea651e3a9d1143d773d1e33087d848da
zach <ztbochanski@gmail.com>
2022-06-22 18:28:47 -0700

    add sidebar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
-import Header from "/src/components/header"
-import Footer from "/src/components/footer"
+import Header from "./header"
+import Footer from "./footer"
+import SideBar from "./sidebar"
 
 const Layout = ({ children }) => {
   const { title } = useSiteMetadata()
   return(
     <div>
       <Header siteTitle={title || `Flying is Rad`} />
+      <SideBar />
       {children}
       <Footer siteTitle={title || `Flying is Rad`} />
     </div>
