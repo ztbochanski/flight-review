ec7ab6a4bbae978a00c8905e31b785d34642a77a
zach <ztbochanski@gmail.com>
2022-06-22 19:10:48 -0700

    add sidebar tree method and map menu items out

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { useMarkdownNodes } from "../hooks/useMarkdownNodes.js"
+import { Link } from "gatsby"
+
+const SideBar = () => {
+
+  const nodes = useMarkdownNodes()
+  const  menuItems = tree(nodes)
+  
+  return(
+    <div className="block m-5">
+      <aside className="menu">
+        {menuItems.map((item, key) => (
+          <div key={key}>
+            <div><p className="menu-label">{item.menuParent}</p></div>
+            <ul className="menu-list">
+              {item.items.map(link => (
+                <div key={link.id}>
+                  <li><Link to={link.slug}>{link.name}</Link></li>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </aside>
+    </div>
+  )
+}
+
+export default SideBar
+
+const tree = (nodes) => {
+  const parents = new Set()
+  nodes.forEach(node => (
+    parents.add(node.frontmatter.parent)
+  ))
+
+  const tree = []
+  parents.forEach(parent => (
+    tree.push({ menuParent: parent, items: [] })
+  ))
+  
+  nodes.forEach(node => {
+    const slug = node.slug
+    const id = node.id
+    const title = node.frontmatter.title
+    const parent = node.frontmatter.parent
+    tree.forEach(item => {
+      if (parent === item.menuParent)
+        item.items.push({ name: title, slug: slug, id: id})
+    })
+  })
+  return tree
+}
